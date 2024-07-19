30fd96db678b6b68ab1881a5091fe169061d03c7
kaleabo <selamawi.gd@gmail.com>
2024-07-19 10:50:26 -0700

    update VideoChat.tsx

--- src/pages/VideoChat.tsx
+++ src/pages/VideoChat.tsx
@@ -1,17 +1,24 @@
-import JitsiMeet from "@/components/JitsiMeet";
-import { useParams } from "react-router-dom";
-import Loading from "@/components/Loading";
+// import JitsiMeet from "@/components/JitsiMeet";
+// import { useParams } from "react-router-dom";
+// import Loading from "@/components/Loading";
+// import React from "react";
+
+// // Define an interface for the VideoChat component props
+
+// const VideoChat: React.FC = () => {
+//   const { roomName } = useParams<{ roomName: string | undefined }>();
+
+//   if (!roomName) return <Loading />;
+
+//   /* Render the JitsiMeet component with the specified RoomName */
+//   return <JitsiMeet roomName={roomName} />;
+// };
+
+// export default VideoChat;
 import React from "react";
 
-// Define an interface for the VideoChat component props
-
 const VideoChat: React.FC = () => {
-  const { roomName } = useParams<{ roomName: string | undefined }>();
-
-  if (!roomName) return <Loading />;
-
-  /* Render the JitsiMeet component with the specified RoomName */
-  return <JitsiMeet roomName={roomName} />;
+  return <div>VideoChat</div>;
 };
 
 export default VideoChat;
